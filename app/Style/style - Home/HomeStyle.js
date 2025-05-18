6040dcd12e28d45465d5283c389e055917245cf6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-18 17:06:53 -0400

    Se agrego el estilo del home

---
+++ app/Style/style - Home/HomeStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const HomeStyle = StyleSheet.create({
+    Container:{
+        justifyContent: 'center',
+        alignItems:'center',
+        gap:20,
+        // paddingVertical:30
+    },
+    ListContainer:{
+        width: '100%',
+        paddingHorizontal: 10,
+    }
+})
