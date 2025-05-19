f892e640c4eaf8d18b5aea59d9842b3e3448029d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-18 20:58:28 -0400

    Nuevas pantallas agregadas

--- app/App.js
+++ app/App.js
@@ -1,11 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import DBZScreen from './Screens/DBZ_Screen';
 import CapitulosAnime from './Screens/CapitulosAnime';
-import HomeScreen from './Screens/Home';
+import HomeScreen, { AboutThisAnime } from './Screens/Home';
 import { Pressable,Text } from 'react-native';
+import AboutAnime from './Screens/AboutAnime';
+import { useEffect, useState } from 'react';
+import Personajes from './Screens/Personajes';
+import AboutPersonajes from './Screens/AboutPersonajes';
 
 export default function App() {
+
+  const [aboutThisAnime,setAboutThisAnime] = useState(null);
 
   const stack = createNativeStackNavigator();
 
@@ -14,8 +19,12 @@ export default function App() {
     <NavigationContainer>
       <stack.Navigator initialRouteName='AnimeUrp'>
         <stack.Screen name='AnimeUrp' component={HomeScreen}/>
-        <stack.Screen name='Dragon Ball' component={DBZScreen}/>
+        <stack.Screen name='AboutAnime' options={({route})=>({
+          title: route.params.AnimeTitle
+        })} component={AboutAnime}/>
         <stack.Screen  name='capitulos' component={CapitulosAnime} />
+        <stack.Screen name='Personajes' component={Personajes}/>
+        <stack.Screen name='AboutPersonajes' component={AboutPersonajes}/>
       </stack.Navigator>
     </NavigationContainer>
   );
