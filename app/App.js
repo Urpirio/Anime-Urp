878f737add2d57a2b90c7f8f386b9c95d50f2fc2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-19 22:44:26 -0400

    Se  agregro  una nueva screen de bloqueo

--- app/App.js
+++ app/App.js
@@ -1,30 +1,41 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CapitulosAnime from './Screens/CapitulosAnime';
-import HomeScreen, { AboutThisAnime } from './Screens/Home';
-import { Pressable,Text } from 'react-native';
+import HomeScreen from './Screens/Home';
 import AboutAnime from './Screens/AboutAnime';
-import { useEffect, useState } from 'react';
 import Personajes from './Screens/Personajes';
 import AboutPersonajes from './Screens/AboutPersonajes';
+import Lock from './Screens/Lock';
+import { View,Text } from 'react-native';
 
 export default function App() {
 
-  const [aboutThisAnime,setAboutThisAnime] = useState(null);
 
   const stack = createNativeStackNavigator();
 
   return (
 
     <NavigationContainer>
-      <stack.Navigator initialRouteName='AnimeUrp'>
-        <stack.Screen name='AnimeUrp' component={HomeScreen}/>
+      <stack.Navigator initialRouteName='Lock'>
+        <stack.Screen name='Lock' options={{
+          header:()=>{}
+        }} component={Lock} />
+        <stack.Screen name='AnimeUrp' options={{
+          headerBackVisible: false,
+        }} component={HomeScreen}/>
+
         <stack.Screen name='AboutAnime' options={({route})=>({
           title: route.params.AnimeTitle
         })} component={AboutAnime}/>
+
         <stack.Screen  name='capitulos' component={CapitulosAnime} />
-        <stack.Screen name='Personajes' component={Personajes}/>
+
+        <stack.Screen name='Personajes' options={({route})=>({
+          title:'Personajes de ' + route.params.AnimeTitle
+        })} component={Personajes}/>
+
         <stack.Screen name='AboutPersonajes' component={AboutPersonajes}/>
+        
       </stack.Navigator>
     </NavigationContainer>
   );
